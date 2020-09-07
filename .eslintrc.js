f9c83d77f7d7b2f2914d7eba5521c102aa6d2c01
yuanmeng0801 <1225610005@qq.com>
2020-09-08 00:13:01 +0800

    配置prettier、eslint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+const OFF = 0
+const WARN = 1
+const ERROR = 2
+
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    },
+  },
+  plugins: ['react', 'unicorn', 'promise', '@typescript-eslint'],
+  rules: {
+    'no-var': OFF,
+    semi: OFF,
+    'linebreak-style': [0, 'error', 'window'],
+    'no-console': 'off',
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+  },
+}
---
+++ lib/icon.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+var a = 1
+var b = 2
+console.log(a, b)
+function Icon() {
+  return (
+    <span>ic1111111111111112222222222222222222222222211111111111111111111111on</span>
+  )
+}
+export default Icon
